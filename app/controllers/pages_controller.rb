aa5a60a096a6d4aa37656deac9e97794ff132946
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2022-05-18 23:07:50 +0200

    ajout des autres pages : cgv, mentions légales, contact, etc.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
-  def accueil
-  end
+  def accueil; end
+
+  def cgv; end
+
+  def contact; end
+
+  def cookies; end
+
+  def mentionslegales; end
+
+  def nettoyagelieux; end
+
+  def soins; end
+
+  def vosquestions; end
 end
