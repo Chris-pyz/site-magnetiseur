89f2602b3e2b5b4de57c67365b1188948a397048
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2022-05-20 22:54:46 +0200

    modif nom des path

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#accueil'
   get 'cgv', to: 'pages#cgv'
-  get 'contact', to: 'pages#contact'
+  get 'contact-magnétiseur', to: 'pages#contact'
   get 'cookies', to: 'pages#cookies'
   get 'mentions-légales', to: 'pages#mentionslegales'
   get 'nettoyage-des-lieux', to: 'pages#nettoyagelieux'
-  get 'soins-magnétisme-marseille', to: 'pages#soins'
+  get 'soins-magnétiseur-marseille', to: 'pages#soins'
   get 'vos-questions', to: 'pages#vosquestions'
   get 'magnétiseur-marseille', to: 'pages#whoiam'
 end
