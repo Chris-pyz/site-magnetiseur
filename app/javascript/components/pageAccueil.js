e3c9e7a5cafaec4c488614549515e4417b433b50
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2022-05-20 17:14:16 +0200

    ajout meta name et property

--- app/javascript/components/pageAccueil.js
+++ app/javascript/components/pageAccueil.js
@@ -1,11 +1,12 @@
 const pageAccueil = () => {
 
-$('.navTrigger').click(function () {
+ $('.navTrigger').click(function () {
   $(this).toggleClass('active');
   console.log("Clicked menu");
   $("#mainListDiv").toggleClass("show_list");
   $("#mainListDiv").fadeIn();
 
 });
+
 }
 export { pageAccueil };
