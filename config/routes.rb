217a4acf856332cbd4b1aa20791140305ae3a6f5
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2022-05-24 17:55:59 +0200

    ajout des link_to

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#accueil'
   get 'cgv', to: 'pages#cgv'
-  get 'contact-magnétiseur', to: 'pages#contact'
+  get 'contact-magnetiseur', to: 'pages#contact'
   get 'cookies', to: 'pages#cookies'
-  get 'mentions-légales', to: 'pages#mentionslegales'
+  get 'mentions-legales', to: 'pages#mentionslegales'
   get 'nettoyage-des-lieux', to: 'pages#nettoyagelieux'
-  get 'soins-magnétiseur-marseille', to: 'pages#soins'
+  get 'soins-magnetiseur-marseille', to: 'pages#soins'
   get 'vos-questions', to: 'pages#vosquestions'
-  get 'magnétiseur-marseille', to: 'pages#whoiam'
+  get 'magnetiseur-marseille', to: 'pages#whoiam'
 end
