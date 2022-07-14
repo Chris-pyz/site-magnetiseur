08079f620e36e4e04cd3d8fb650db5d54eea64b0
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2022-07-14 17:29:58 +0200

    suppr des doublons seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Temoignage.create(prenom: 'anna', nom: 'meroine', email: 'anna@lewagon.com', commentaires: "Merci Christophe pour ce
-  soin énergétique qui m'a  fait un très grand bien. Ma vie s'en est d'ailleur améliorée, je me sens alignée.")
-
-Temoignage.create(prenom: 'Denis', nom: 'hadjal', email: 'anna@lewagon.com', commentaires: "Merci Christophe
-  pour ce massage qui m'a  fait un très grand bien. Je me sens plus zen. dfghjiip ergrez zeg xcvsdqfg zzeght agf
-  arg hrh fdgtr eyjyt '(tkitultd jhtyjkukr ")
-
-Temoignage.create(prenom: 'bernard', nom: 'Déroudhile', email: 'anna@lewagon.com', commentaires: "Merci Christophe
-  pour ce massage shiatsu qui m'a  fait un très grand. Je me sens plus moi. fgn dfhyj zeh jty1h zrjhz thyt zt
-  zh ytkteyjrt h,rtj n,tsg ilgj ; tistr ykryuku lryuk j k èilkstul j k")
-
-Temoignage.create(prenom: 'Marie-Pierre', nom: 'Régoli', email: 'anna@lewagon.com', commentaires: "Merci Christophe
-  pour ce soin qui m'a  fait un très grand. Je me sens plus zen. qer hyk i th 'yj; hjhg 000000000000000 rth ejuk zzeght jejjej ")
